80d9e072c6842c1fa81576790459f04147ddb2bd
tanvir18009 <tanvir1807009@stud.kuet.ac.bd>
2022-04-28 19:20:21 +0600

    add NewFile.java containing getter, setter, constructors

---
+++ NewFile.java
@@ -0,0 +1,23 @@
+package com.github.characterdog.bmicalculator;
+
+public class newfile {
+    String name, age;
+    public newfile() {
+    }
+
+    public String getName{
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
