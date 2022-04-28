f6ade3df969fffd4edbf8642b90db58a246da9f3
tanvir18009 <tanvir1807009@stud.kuet.ac.bd>
2022-04-28 19:30:22 +0600

    add NewFileBranch.java from newBranch

---
+++ NewFileBranch.java
@@ -0,0 +1,23 @@
+package com.github.characterdog.bmicalculator;
+
+public class newFileBranch {
+    String name, age;
+    public newFileBranch() {
+    }
+
+    public String getName{
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
